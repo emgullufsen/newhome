0e433950ad245e2420e544bb2132863438a54f02
rickygee <ericgullufsen@gmail.com>
2020-09-03 16:30:57 -0800

    fugg

--- footie/games-today.php
+++ footie/games-today.php
@@ -1,15 +1,12 @@
 <html>
 <head>
-<<<<<<< HEAD
 	<title>&#x26bd; Footie Today</title>
 	<link href="css/games-today-styles.css" rel="stylesheet">
-=======
 	<meta charset="utf-8">
 	<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%2290%22 font-size=%2290%22>&#x26bd;</text></svg>">
     
     <link href="style/games-today.css" rel="stylesheet" type="text/css">
 	<title>Footie Today</title>
->>>>>>> 040b93c7c7db39ce632fa7d2942b4973027bc5b7
 </head>
 <body>
 	<a href="../newhome.html">
