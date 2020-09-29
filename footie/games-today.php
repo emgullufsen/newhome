f2a85afd0052c09b047b006aac744e59dcd417c6
Eric Gullufsen <eric.gullufsen@alaska.gov>
2020-09-28 16:32:39 -0800

    lil fetch stuff weathcer

--- footie/games-today.php
+++ footie/games-today.php
@@ -3,7 +3,8 @@
 	<title>Games Today</title>
 	<meta charset="utf-8">
 	<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%2290%22 font-size=%2290%22>&#x26bd;</text></svg>">
-    	<link href="style/games-today.css" rel="stylesheet" type="text/css">
+	<link href="style/games-today.css" rel="stylesheet" type="text/css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 	<a href="../newhome.html">
